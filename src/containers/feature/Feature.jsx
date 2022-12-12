d5d414a6e65ae9eab238fb06b055aaa2daacb98d
518651 <1725212799@qq.com>
2022-12-13 00:07:07 +0800

    modified:   src/components/feature/Feature.jsx 	modified:   src/components/feature/feature.css 	modified:   src/containers/feature/Feature.jsx 	modified:   src/containers/feature/feature.css

--- src/containers/feature/Feature.jsx
+++ src/containers/feature/Feature.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = ({title,text}) => {
-  <div className='gpt3__feature-container__feature'>
-    <div className='gpt3__feature-container__feature-title'>
-      <div/>
-      <h1>{title}</h1>
+const Feature = () => {
+  return (
+    <div>
+      Feature
     </div>
-    <div className='gpt3__feature-container_feature_text'>
-      <p>{text}</p>
-    </div>
-  </div>
+  )
 }
 
 export default Feature
-  
